aec27d5540e8aa307e3cd92c80b9be835da9ed55
Sarkar-shreyas <ssarkar20001@gmail.com>
2025-11-07 21:10:47 +0000

    Updated it to be more reusable between different versions

--- test_data.py
+++ test_data.py
@@ -9,91 +9,113 @@ from Taskfarm.source.utilities import (
 )
 import os
 import sys
+from collections import defaultdict
 
 DATA_DIR = "C:/Users/ssark/Desktop/Uni/Year 4 Courses/Physics Final Year Project/Project Code/Data from taskfarm"
 
 
-def load_hist_data(filename: str) -> list:
+def load_hist_data(filename: str) -> tuple:
     data = np.load(filename)
     counts = data["histval"]
     bins = data["binedges"]
     centers = data["bincenters"]
 
-    return [counts, bins, centers]
+    return (counts, bins, centers)
+
+
+def load_moments(filename: str) -> tuple:
+    data = np.load(filename)
+    l2 = data["l2_dist"]
+    mean = data["mean"]
+    std = data["std"]
+    conv = data["converged"]
+    return (l2, mean, std, conv)
 
 
 if __name__ == "__main__":
-    version = 1
-    var_names = ["t", "g", "z"]
-    rgs = list(range(10))
-    data = {}
+    version = 1.1
+    N = 120000000
+    var_names = ["t", "g", "z", "input_t"]
+    hist_dir = f"{DATA_DIR}/v{version}/hist"
+    stats_dir = f"{DATA_DIR}/v{version}/stats"
+    plots_dir = f"{DATA_DIR}/v{version}/plots"
+    t_folder = f"{hist_dir}/t"
+    g_folder = f"{hist_dir}/g"
+    z_folder = f"{hist_dir}/z"
+    input_folder = f"{hist_dir}/input"
+    sym_folder = f"{DATA_DIR}/v{version}/hist/sym"
+    folder_names = {
+        "t": t_folder,
+        "g": g_folder,
+        "z": z_folder,
+        "input_t": input_folder,
+        "sym": sym_folder,
+    }
+    num_steps = 8
+    map = defaultdict(list)
     for var in var_names:
-        for rg in rgs:
-            filename = f"{DATA_DIR}/v{version}/data/hist/{var}_hist_RG{rg}.npz"
-            sym_filename = f"{DATA_DIR}/v{version}/data/hist/{var}_hist_RG{rg}_sym.npz"
+        for i in range(num_steps):
+            file = f"{folder_names[var]}/{var}_hist_RG{i}.npz"
+            counts, bins, centers = load_hist_data(file)
+            densities = get_density(counts, bins)
+            map[var].append([densities, bins, centers])
 
-            # print(filename)
-            if os.path.exists(filename):
-                data[f"{var}_RG{rg}"] = load_hist_data(filename)
-            if os.path.exists(sym_filename):
-                data[f"{var}_RG{rg}_sym"] = load_hist_data(sym_filename)
+    for i in range(num_steps):
+        file = f"{folder_names['sym']}/z_hist_RG{i}_sym.npz"
+        counts, bins, centers = load_hist_data(file)
+        densities = get_density(counts, bins)
+        map["sym"].append([densities, bins, centers])
 
-    bins = data["t_RG0"][2]
-    # print(data.keys())
+    # print(len(map["t"][0]))
+    # for key in map:
+    #     plt.figure()
+    #     plt.title(f"Histogram of dataset {key}")
+    #     plt.xlabel(f"{key}")
+    #     plt.ylabel(f"P({key})")
+    #     for i in range(num_steps):
+    #         plt.plot(map[key][i][1], map[key][i][0], label=f"RG{i}")
+    #     plt.legend()
+    #     plt.savefig(f"{DATA_DIR}/v{version}/plots/{key}_histogram", dpi=150)
 
-    plt.title("z histogram")
+    moments = defaultdict(list)
+    for i in range(num_steps):
+        file = f"{stats_dir}/z_moments_RG{i}_{N}_samples.npz"
+        l2, mean, std, converged = load_moments(file)
+        moments[i].append([l2, mean, std, converged])
+
+    # print(moments)
+    plt.figure()
+    plt.title("Histogram of Unymmetrised z data")
     plt.xlabel("z")
     plt.ylabel("Q(z)")
-    plt.xlim([-1, 3])
-    plt.ylim([0.1, 0.3])
-    for key in data.keys():
-        a = get_density(data[key][0], data[key][1])
-        if str(key)[0] == "z" and str(key)[-1] != "m":
-            plt.plot(data[key][2], a, label=f"{key}")
-    # n = [0, 2, 4, 8]
-    # for i in n:
-    #     filename = (
-    #         f"{DATA_DIR}/v{version}/data/laundered t/t_laundered_RG{i}_batch_8.txt"
-    #     )
-    #     if os.path.exists(filename):
-    #         input_t = np.loadtxt(filename)
-    #         print("Loaded data")
-    #         print(input_t.size)
-    #         hist, _ = np.histogram(input_t, 1000, range=(0, 1), density=True)
-    #         plt.plot(bins, hist, label=f"RG{i}")
-
-    f = l2_distance(
-        data["z_RG7"][0],
-        data["z_RG8"][0],
-        data["z_RG7"][1],
-        data["z_RG8"][1],
-    )
-    mean_7, std_7 = hist_moments(data["z_RG7"][0], data["z_RG7"][1])
-    mean_8, std_8 = hist_moments(data["z_RG8"][0], data["z_RG8"][1])
-    print(f"L2 distance between unsymmetrised RG7&RG8: {f:.7f}")
-    print(f"Unsymmetrised RG7 moments: Mean = {mean_7:.5f}, STD = {std_7:.7f}")
-    print(f"Unsymmetrised RG8 moments: Mean = {mean_8:.5f}, STD = {std_8:.7f}")
-    if f <= DIST_TOLERANCE:
-        print("Unsymmetrised Dist Converged!")
-    if np.abs(std_8 - std_7) <= STD_TOLERANCE:
-        print("Unsymmetrised STD Converged!")
-    print("=" * 100)
-    f_sym = l2_distance(
-        data["z_RG7_sym"][0],
-        data["z_RG8_sym"][0],
-        data["z_RG7_sym"][1],
-        data["z_RG8_sym"][1],
-    )
-    mean_7_sym, std_7_sym = hist_moments(data["z_RG7_sym"][0], data["z_RG7_sym"][1])
-    mean_8_sym, std_8_sym = hist_moments(data["z_RG8_sym"][0], data["z_RG8_sym"][1])
-    print("=" * 100)
-    print(f"L2 distance between symmetrised RG7&RG8: {f_sym:.7f}")
-    print(f"Symmetrised RG7 moments: Mean = {mean_7_sym}, STD = {std_7_sym:.7f}")
-    print(f"Symmetrised RG8 moments: Mean = {mean_8_sym}, STD = {std_8_sym:.7f}")
-    if f_sym <= DIST_TOLERANCE:
-        print("Symmetrised Dist Converged!")
-    if np.abs(std_8_sym - std_7_sym) <= STD_TOLERANCE:
-        print("Symmetrised STD Converged!")
-    print("=" * 100)
+    plt.xlim([-2, 4])
+    plt.ylim([0, 0.3])
+    dist = []
+    moments = []
+    for i in range(num_steps):
+        if i > 0:
+            dist.append(
+                l2_distance(
+                    map["z"][i][0],
+                    map["z"][i - 1][0],
+                    map["z"][i][1],
+                    map["z"][i - 1][1],
+                )
+            )
+        moments.append(hist_moments(map["z"][i][0], map["z"][i][1]))
+        plt.plot(map["z"][i][2], map["z"][i][0], label=f"RG{i}")
     plt.legend()
-    # plt.savefig(f"{DATA_DIR}/v{version}/data/plots/input_t_hist", dpi=150)
+    plt.savefig(f"{plots_dir}/z_truncated.png", dpi=150)
+
+    for i in range(num_steps - 1):
+        print(f"L2 distance between RG{i} and RG{i + 1} = {dist[i]}")
+        if dist[i] <= DIST_TOLERANCE:
+            print(f"The histogram converged for L2 distance by RG step {i + 1}!")
+
+    for i in range(num_steps):
+        mean, std = moments[i]
+        print(f"Mean and STD for RG{i}: Mean = {mean:.7f}, STD = {std:.7f}")
+        if i > 0:
+            _, prev_std = moments[i - 1]
+            if np.abs(std - prev_std) <= STD_TOLERANCE:
+                print(f"The histogram converged for STD by RG step {i}")
