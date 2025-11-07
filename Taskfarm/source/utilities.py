Fixed L2 distance calculation algorithm

Taskfarm/source/utilities.py
@@ -631,14 +631,13 @@ def l2_distance(old_hist_val, new_hist_val, old_bins, new_bins) -> float:
     """
     old_density = get_density(old_hist_val, old_bins)
     new_density = get_density(new_hist_val, new_bins)
-    integrand = (new_density - old_density) ** 2
-    # integrand = np.abs(new_density**2 - old_density**2)
-    # num_bins = len(old_hist_val)
-    dz = np.diff(new_bins)
-    bin_sum = np.sum(dz)
-    # distance = float(np.sum(np.sqrt(integrand) * dz))
-    distance = float(np.sqrt(np.sum(integrand * dz) / bin_sum))
-    return distance
+    shaw_density_integrand = np.abs(new_density**2 - old_density**2)
+    num_bins = len(old_hist_val)
+    old_dz = np.diff(old_bins)
+    density_shaw_distance = float(
+        np.sum(np.sqrt(shaw_density_integrand) * old_dz) / num_bins
+    )
+    return density_shaw_distance
 
 
 def hist_moments(hist_vals: np.ndarray, bins: np.ndarray) -> tuple: