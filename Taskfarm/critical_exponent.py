0d1544e7840d72f4bfdb7847c686da9ab2f36a04
Sarkar-shreyas <ssarkar20001@gmail.com>
2025-11-11 09:18:40 +0000

    v1.4: changed peak calculation to take the difference instead of pure peaks, added the 0 shift to compare to unperturbed behaviour

--- Taskfarm/critical_exponent.py
+++ Taskfarm/critical_exponent.py
@@ -16,10 +16,10 @@ import json
 from time import time
 
 DATA_DIR = "C:/Users/ssark/Desktop/Uni/Year 4 Courses/Physics Final Year Project/Project Code/Taskfarm/Data from taskfarm"
-CURRENT_VERSION = 1.31
+CURRENT_VERSION = 1.4
 TYPE = "EXP"
-NUM_RG = 8
-SHIFTS = [0.003, 0.005, 0.007, 0.009]
+NUM_RG = 7
+SHIFTS = [0.0, 0.003, 0.005, 0.007, 0.009]
 
 if __name__ == "__main__":
     version = CURRENT_VERSION
@@ -42,18 +42,18 @@ if __name__ == "__main__":
             ax1.set_xlabel(f"{var}")
             ax1.set_ylabel(f"P({var})")
             if var == "z":
-                ax0.set_xlim([-6.0, 26.0])
+                ax0.set_xlim([-50, 50])
                 ax0.set_ylim([0.0, 0.3])
-                ax1.set_xlim([-1.0, 26.0])
+                ax1.set_xlim([-25, 25.0])
                 ax1.set_ylim([0, 0.3])
             else:
                 ax0.set_ylim([0.0, 2.0])
                 ax1.set_ylim([0.0, 2.0])
-            shift_dir = f"{DATA_DIR}/v{version}/{TYPE}/shift_{shift}/{var}"
+            shift_dir = f"{DATA_DIR}/v{version}/{TYPE}/shift_{shift}/hist/{var}"
             shift_plot_dir = f"{plots_dir}/{shift}"
-            shift_stats_dir = f"{stats_dir}/{shift}"
+            # shift_stats_dir = f"{stats_dir}/{shift}"
             os.makedirs(shift_plot_dir, exist_ok=True)
-            os.makedirs(shift_stats_dir, exist_ok=True)
+            # os.makedirs(shift_stats_dir, exist_ok=True)
             for i in range(NUM_RG):
                 filename = f"{shift_dir}/{var}_hist_RG{i}.npz"
                 counts, bins, centers = load_hist_data(filename)
@@ -69,7 +69,7 @@ if __name__ == "__main__":
         print(f"Finished plotting data of {var} for all shifts")
 
     print("All plots made.")
-    print(map.keys())
+    # print(map.keys())
     fig, (ax_0, ax_1) = plt.subplots(1, 2, figsize=(10, 4))
     ax_0.set_xlim([0, 0.01])
     # ax_0.set_ylim([0.0, 2])
@@ -81,12 +81,14 @@ if __name__ == "__main__":
     ax_1.set_ylabel("z_peak")
     ax_1.set_xlim([0, 0.01])
     # ax_1.set_ylim([0, 2])
-    z_moments = []
-    z_dist = []
+
     peaks = np.zeros((NUM_RG, len(SHIFTS))).astype(float)
     print("Beginning peak estimations")
     print("-" * 100)
+
     for j in range(len(SHIFTS)):
+        z_moments = []
+        z_dist = []
         shift = SHIFTS[j]
         print(f"Estimating peak for shift {shift}")
         loop = time()
@@ -107,19 +109,19 @@ if __name__ == "__main__":
             print(
                 f"Peak estimated for RG{i} of shift {shift} after {time() - loop:.3f} seconds"
             )
-        z_stats = f"{stats_dir}/{shift}/z_stats.json"
-        construct_moments_dict(z_stats, z_moments, z_dist)
+        z_stats = f"{stats_dir}/{shift}_z_stats.json"
+        construct_moments_dict(z_stats, z_moments, z_dist, NUM_RG)
         print(f"Stats for shift {shift} saved to {z_stats}")
         print("-" * 100)
     print(
         f"Peaks estimated for each shift at every RG {time() - start:.3f} seconds from start of program"
     )
-
+    print(z_moments)
     overall_stats = defaultdict(dict)
     overall_stats_file = f"{stats_dir}/overall_stats.json"
     x = np.array(SHIFTS).astype(float)
-    for i in range(6):
-        y = peaks[i, :]
+    for i in range(1, 6):
+        y = peaks[i, :] - peaks[0, :]
         slope, r2 = fit_z_peaks(x, y)
         ax_0.scatter(x, y, label=f"RG_{i}")
         ax_1.scatter(x, y, label=f"RG_{i}")
