4fcf7b3ae5e41c6b26e72b0a12f55c3e00632e32
Sarkar-shreyas <ssarkar20001@gmail.com>
2025-11-23 18:36:26 +0000

    Fixed legend locations for t plots

--- Taskfarm/constants.py
+++ Taskfarm/constants.py
@@ -1,9 +1,11 @@
 N = 320000000
 DATA_DIR = "C:/Users/ssark/Desktop/Uni/Year 4 Courses/Physics Final Year Project/Project Code/Taskfarm/Data from taskfarm"
-CURRENT_VERSION = "1.81C"
-NUM_RG = 9
+CURRENT_VERSION = "1.84S"
+# NUM_RG = 9
+NUM_RG = 10
 # NUM_RG = 12
-SHIFTS = [0.0, 0.003, 0.005, 0.007, 0.009]
+# SHIFTS = [0.0, 0.003, 0.005, 0.007, 0.009]
+SHIFTS = ["0.0", "0.003", "0.005", "0.0075", "0.010"]
 # SHIFTS = ["0.0", "0.003", "0.004", "0.005", "0.006", "0.0075", "0.010"]
 
 LEGENDS = {
@@ -15,9 +17,9 @@ LEGENDS = {
         "sym_z": "upper left",
     },
     "EXP": {
-        "t": "upper right",
+        "t": "upper left",
         "g": "upper right",
-        "input_t": "upper right",
+        "input_t": "upper left",
         "z": "upper left",
     },
 }
