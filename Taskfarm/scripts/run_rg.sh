2e3fe478d1e351f036ffdac7536f662b6868a0b7
Sarkar-shreyas <ssarkar20001@gmail.com>
2025-12-20 05:18:40 +0000

    Early stage CLI driver for RG fp runs

--- Taskfarm/scripts/run_rg.sh
+++ Taskfarm/scripts/run_rg.sh
@@ -5,11 +5,12 @@ VERSION=2.10
 TYPE=FP
 METHOD="a"
 EXPR="s"
+SAMPLER="i"
 N=480000000
 K=10
 
 # Read command line input
-while getopts 'nk:m:e:' OPTION; do
+while getopts 'nk:m:e:s:' OPTION; do
     case "${OPTION}" in
         n)
             N=$OPTARG
@@ -27,6 +28,10 @@ while getopts 'nk:m:e:' OPTION; do
             EXPR=$OPTARG
             echo "Proceeding with ${EXPR}'s expression"
             ;;
+        s)
+            SAMPLER=$OPTARG
+            echo "Proceeding with ${SAMPLER} sampling method"
+            ;;
         h)
             echo "==========================================================================="
             echo "                  RG SCRIPT HELPER "
@@ -35,6 +40,7 @@ while getopts 'nk:m:e:' OPTION; do
             echo " -k : Number of RG steps to run "
             echo " -m : Method of analysis (a = analytic, n = numerical)"
             echo " -e : Expression to use for analytic method (c = Cain, s = Shaw, j = Jack) "
+            echo " -s : Sampler method (r = rejection, i = inverse CDF) "
             echo "==========================================================================="
             echo ""
     esac
@@ -47,8 +53,9 @@ echo " Version            : $VERSION "
 echo " Type               : $TYPE "
 echo " Number of samples  : $N "
 echo " Number of RG steps : $K "
-echo " Method             : $METHOD "
+echo " t' Method          : $METHOD "
 echo " Expression         : $EXPR "
+echo " Sampler method : $SAMPLER "
 echo " Date of job        : [$(date '+%Y-%m-%d %H:%M:%S')] "
 echo "====================================================="
 echo ""
@@ -57,6 +64,6 @@ VERSIONSTR="${VERSION}${EXPR}"
 
 rg_job=$(sbatch --parsable \
         "rg_fp_master.sh" \
-        "$VERSIONSTR" "$N" "$K" "$METHOD" "$EXPR")
+        "$VERSIONSTR" "$N" "$K" "$METHOD" "$EXPR" "$SAMPLER")
 
 echo " [$(date '+%Y-%m-%d %H:%M:%S')]: Submitted RG job with id $rg_job "
