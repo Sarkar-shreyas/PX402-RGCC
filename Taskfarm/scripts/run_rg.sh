b1be6156bd5209376e3c625b8c00244c41dcac89
Sarkar-shreyas <ssarkar20001@gmail.com>
2025-12-27 21:14:52 +0800

    Added CLI parsing scripts for starting RG runs, one for FP runs and the other for EXP runs

--- Taskfarm/scripts/run_rg.sh
+++ Taskfarm/scripts/run_rg.sh
@@ -5,44 +5,65 @@ VERSION=2.10
 TYPE=FP
 METHOD="a"
 EXPR="s"
-SAMPLER="i"
+LAUNDER="i"
+SYMMETRISE=1
 N=480000000
+SEED=10
 K=10
 
+set -euo pipefail
+
 # Read command line input
-while getopts 'nk:m:e:s:' OPTION; do
+while getopts 'v:n:k:m:e:l:s:y:h' OPTION; do
     case "${OPTION}" in
+        v)
+            VERSION="$OPTARG" ;;
         n)
-            N=$OPTARG
-            echo "Proceeding with $N samples"
-            ;;
+            N="$OPTARG" ;;
         k)
-            K=$OPTARG
-            echo "Proceeding with $K RG steps"
-            ;;
+            K="$OPTARG" ;;
         m)
-            METHOD="$OPTARG"
-            echo "Proceeding with the $METHOD method"
-            ;;
+            METHOD="$OPTARG" ;;
         e)
-            EXPR=$OPTARG
-            echo "Proceeding with ${EXPR}'s expression"
-            ;;
+            EXPR="$OPTARG" ;;
+        l)
+            LAUNDER="$OPTARG" ;;
         s)
-            SAMPLER=$OPTARG
-            echo "Proceeding with ${SAMPLER} sampling method"
-            ;;
+            SEED="$OPTARG" ;;
+        y)
+            SYMMETRISE="$OPTARG" ;;
         h)
-            echo "==========================================================================="
-            echo "                  RG SCRIPT HELPER "
-            echo "---------------------------------------------------------------------------"
-            echo " -n : Number of samples to generate "
-            echo " -k : Number of RG steps to run "
-            echo " -m : Method of analysis (a = analytic, n = numerical)"
-            echo " -e : Expression to use for analytic method (c = Cain, s = Shaw, j = Jack) "
-            echo " -s : Sampler method (r = rejection, i = inverse CDF) "
-            echo "==========================================================================="
-            echo ""
+            echo "============================================================================="
+            echo "                              RG SCRIPT HELPER "
+            echo "-----------------------------------------------------------------------------"
+            echo " -v   : Version/run name "
+            echo " -n   : Number of samples to generate "
+            echo " -k   : Number of RG steps to run "
+            echo " -s   : Starting seed value "
+            echo " -m   : Method of computation (a = analytic, n = numerical)"
+            echo " -e   : Expression to use for analytic method (c = Cain, s = Shaw, j = Jack) "
+            echo " -l   : Launder method (r = rejection, i = inverse CDF) "
+            echo " -y   : Symmetrise z data per step? (1 = yes, 0 = no)"
+            echo " -h   : help "
+            echo "============================================================================="
+            echo "";
+            exit 0 ;;
+        *)
+            echo "============================================================================="
+            echo "                              RG SCRIPT HELPER "
+            echo "-----------------------------------------------------------------------------"
+            echo " -v   : Version/run name "
+            echo " -n   : Number of samples to generate "
+            echo " -k   : Number of RG steps to run "
+            echo " -s   : Starting seed value "
+            echo " -m   : Method of computation (a = analytic, n = numerical)"
+            echo " -e   : Expression to use for analytic method (c = Cain, s = Shaw, j = Jack) "
+            echo " -l   : Launder method (r = rejection, i = inverse CDF) "
+            echo " -y   : Symmetrise z data per step? (1 = yes, 0 = no)"
+            echo " -h   : help "
+            echo "============================================================================="
+            echo "";
+            exit 2 ;;
     esac
 done
 
@@ -53,9 +74,11 @@ echo " Version            : $VERSION "
 echo " Type               : $TYPE "
 echo " Number of samples  : $N "
 echo " Number of RG steps : $K "
+echo " Starting seed      : $SEED "
 echo " t' Method          : $METHOD "
 echo " Expression         : $EXPR "
-echo " Sampler method : $SAMPLER "
+echo " Launder method     : $LAUNDER "
+echo " Symmetrising?      : $SYMMETRISE "
 echo " Date of job        : [$(date '+%Y-%m-%d %H:%M:%S')] "
 echo "====================================================="
 echo ""
@@ -64,6 +87,6 @@ VERSIONSTR="${VERSION}${EXPR}"
 
 rg_job=$(sbatch --parsable \
         "rg_fp_master.sh" \
-        "$VERSIONSTR" "$N" "$K" "$METHOD" "$EXPR" "$SAMPLER")
+        "$VERSIONSTR" "$N" "$K" "$SEED" "$METHOD" "$EXPR" "$LAUNDER" "$SYMMETRISE" )
 
 echo " [$(date '+%Y-%m-%d %H:%M:%S')]: Submitted RG job with id $rg_job "
--- Taskfarm/scripts/run_shifts.sh
+++ Taskfarm/scripts/run_shifts.sh
@@ -1,9 +1,100 @@
 #!/bin/bash
 
+# Set default params
+VERSION=2.10
+TYPE=EXP
+METHOD="a"
+EXPR="s"
+LAUNDER="i"
+SYMMETRISE=1
+N=480000000
+K=10
+SEED=10
+FP_NUM=$K
+
 shifts=( 0.003 0.004 0.005 0.006 0.0075 0.010 )
+
+set -euo pipefail
+
+# Read command line input
+while getopts 'v:n:k:s:f:m:e:l:y:h' OPTION; do
+    case "${OPTION}" in
+        v)
+            VERSION="$OPTARG" ;;
+        n)
+            N="$OPTARG" ;;
+        k)
+            K="$OPTARG" ;;
+        s)
+            SEED="$OPTARG" ;;
+        m)
+            METHOD="$OPTARG" ;;
+        e)
+            EXPR="$OPTARG" ;;
+        l)
+            LAUNDER="$OPTARG" ;;
+        y)
+            SYMMETRISE="$OPTARG" ;;
+        f)
+            FP_NUM="$OPTARG" ;;
+        h)
+            echo "============================================================================="
+            echo "                  RG SCRIPT HELPER "
+            echo "-----------------------------------------------------------------------------"
+            echo " -v   : Version/run name "
+            echo " -n   : Number of samples to generate "
+            echo " -k   : Number of RG steps to run "
+            echo " -s   : Starting seed value "
+            echo " -f   : Step no. for FP distribution "
+            echo " -m   : Method of computation (a = analytic, n = numerical)"
+            echo " -e   : Expression to use for analytic method (c = Cain, s = Shaw, j = Jack) "
+            echo " -l   : Launder method (r = rejection, i = inverse CDF) "
+            echo " -y   : Symmetrise z data per step? (1 = yes, 0 = no)"
+            echo " -h   : help "
+            echo "============================================================================="
+            echo "";
+            exit 0 ;;
+        *)
+            echo "============================================================================="
+            echo "                  RG SCRIPT HELPER "
+            echo "-----------------------------------------------------------------------------"
+            echo " -v   : Version/run name "
+            echo " -n   : Number of samples to generate "
+            echo " -k   : Number of RG steps to run "
+            echo " -s   : Starting seed value "
+            echo " -f   : Step no. for FP distribution "
+            echo " -m   : Method of computation (a = analytic, n = numerical)"
+            echo " -e   : Expression to use for analytic method (c = Cain, s = Shaw, j = Jack) "
+            echo " -l   : Launder method (r = rejection, i = inverse CDF) "
+            echo " -y   : Symmetrise z data per step? (1 = yes, 0 = no)"
+            echo " -h   : help "
+            echo "============================================================================="
+            echo "";
+            exit 2 ;;
+    esac
+done
+
+echo "====================================================="
+echo "                  RG WORKFLOW CONFIG "
+echo "-----------------------------------------------------"
+echo " Version            : $VERSION "
+echo " Type               : $TYPE "
+echo " Number of samples  : $N "
+echo " Number of RG steps : $K "
+echo " Starting seed      : $SEED "
+echo " Prev FP step no.   : $FP_NUM "
+echo " t' Method          : $METHOD "
+echo " Expression         : $EXPR "
+echo " Launder method     : $LAUNDER "
+echo " Symmetrising?      : $SYMMETRISE "
+echo " Shifts to apply    : ${shifts[*]} "
+echo " Date of job        : [$(date '+%Y-%m-%d %H:%M:%S')] "
+echo "====================================================="
+echo ""
+
+VERSIONSTR="${VERSION}${EXPR}"
 
 shift_job=$(sbatch --parsable \
     shifted_rg.sh \
-        "${shifts[@]}")
+        "$VERSIONSTR" "$N" "$K" "$SEED" "$FP_NUM" "$METHOD" "$EXPR" "$LAUNDER" "$SYMMETRISE" "${shifts[@]}")
 echo "Submitted shift job ${shift_job} for shift ${shifts[0]}, remaining shifts are: ${shifts[*]:1}"
-
