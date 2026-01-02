56cb8d386fb5f38ebb62e6832a83edee2c3fcb2e
Sarkar-shreyas <ssarkar20001@gmail.com>
2026-01-02 12:26:42 +0800

    Added input validation helper

--- source/parse_config.py
+++ source/parse_config.py
@@ -9,14 +9,29 @@ from source.config import (
     get_nested_data,
     save_updated_config,
 )
+import os
 
 
-def get_project_root() -> Path:
+# ---------- Helper functions ---------- #
+def get_project_root(outer_dirs: int = 2) -> Path:
     """Return the root directory for this project"""
     # from fyp/code/source -> fyp
-    return Path(__file__).resolve().parents[2]
+    return Path(__file__).resolve().parents[outer_dirs]
 
 
+def get_default_output_dir(config: dict, run_type: str) -> Path:
+    """Parse the input config dict and build the default output path"""
+    version = str(get_nested_data(config, "main.version"))
+    method = str(get_nested_data(config, "engine.method"))
+    expr = str(get_nested_data(config, "engine.expr")).strip().lower()
+    version_str = f"{version}_{method}_{expr}"
+
+    root = get_project_root()
+
+    return root / "job_outputs" / version_str / run_type / "config"
+
+
+# ---------- Parsing helpers ---------- #
 def build_parser() -> argparse.ArgumentParser:
     """Build and return the parser for the local RG engine"""
     parser = argparse.ArgumentParser()
@@ -33,31 +48,51 @@ def build_parser() -> argparse.ArgumentParser:
     )
     parser.add_argument("--out", default=None, help="Output path for config")
     parser.add_argument(
-        "--type", required=True, choices=["FP", "EXP"], help="Type of RG workflow"
+        "--type",
+        required=True,
+        default="FP",
+        choices=["FP", "EXP"],
+        help="Type of RG workflow",
     )
 
     return parser
 
 
-def get_default_output_dir(config: dict, run_type: str) -> Path:
-    """Parse the input config dict and build the default output path"""
-    version = str(get_nested_data(config, "main.version"))
-    method = str(get_nested_data(config, "engine.method"))
-    expr = str(get_nested_data(config, "engine.expr")).strip().lower()
-    version_str = f"{version}_{method}_{expr}"
+def validate_input(input_args) -> dict:
+    """Validates input parser args"""
+    args_dict = {}
+    # Validate config path
+    config_path = str(input_args.config).strip()
+    if "." not in config_path:
+        config_path += ".yaml"
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Could not find config file at {config_path}")
+    args_dict["config"] = config_path
 
-    root = get_project_root()
-
-    return root / "job_outputs" / version_str / run_type / "config"
+    # Check and validate output path
+    if input_args.out is not None:
+        output_path = str(input_args.out).strip()
+        if not os.path.isdir(output_path):
+            raise FileNotFoundError(f"Could not find output directory {output_path}")
+        args_dict["out"] = output_path
+    else:
+        args_dict["out"] = None
+    # Check and validate type input
+    rg_type = str(input_args.type).strip().upper()
+    if rg_type not in ("FP", "EXP"):
+        raise ValueError(f"Invalid RG type {rg_type} entered, expected 'FP' or 'EXP'.")
+    args_dict["type"] = rg_type
+    return args_dict
 
 
 if __name__ == "__main__":
     parser = build_parser()
     args = parser.parse_args()
-    config = handle_config(args.config, args.override)
+    args_dict = validate_input(args)
+    config = handle_config(args_dict["config"], args.override)
 
     if args.out is None:
-        output_dir = get_default_output_dir(config, args.type)
+        output_dir = get_default_output_dir(config, args_dict["type"])
     else:
         output_dir = Path(args.out)
 
